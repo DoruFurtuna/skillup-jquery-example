7ac0241dbb17def091a9f64b78f4f799fc067796
Doru Furtuna <doru.furtuna@gmail.com>
2019-08-07 20:52:01 +0300

    Fixed JS error.

--- scripts.js
+++ scripts.js
@@ -1,41 +1,41 @@
-$( function() {
-  var dialog, form,
+$(function() {
+  var dialog,
+    form,
+    name = $("#name"),
+    email = $("#email"),
+    password = $("#password"),
+    allFields = $([])
+      .add(name)
+      .add(email)
+      .add(password);
 
-    name = $( "#name" ),
-    email = $( "#email" ),
-    password = $( "#password" ),
-    allFields = $( [] ).add( name ).add( email ).add( password ),
-
-  function checkLength( input, min, max ) {
-    if ( input.val().length > max || input.val().length < min ) {
-      input.addClass( "ui-state-error" );
+  function checkLength(input, min, max) {
+    if (input.val().length > max || input.val().length < min) {
+      input.addClass("ui-state-error");
       return false;
     } else {
       return true;
     }
   }
 
-
   function addUser() {
     var valid = true;
-    allFields.removeClass( "ui-state-error" );
+    allFields.removeClass("ui-state-error");
 
-    valid = valid && checkLength( name,  3, 16 );
-    valid = valid && checkLength( email, 6, 80 );
-    valid = valid && checkLength( password, 5, 16 );
+    valid = valid && checkLength(name, 3, 16);
+    valid = valid && checkLength(email, 6, 80);
+    valid = valid && checkLength(password, 5, 16);
 
-    if ( valid ) {
-      $( "#users tbody" ).append( "<tr>" +
-        "<td>" + name.val() + "</td>" +
-        "<td>" + email.val() + "</td>" +
-        "<td>" + password.val() + "</td>" +
-      "</tr>" );
-      dialog.dialog( "close" );
+    if (valid) {
+      $("#users tbody").append(
+        "<tr>" + "<td>" + name.val() + "</td>" + "<td>" + email.val() + "</td>" + "<td>" + password.val() + "</td>" + "</tr>"
+      );
+      dialog.dialog("close");
     }
     return valid;
   }
 
-  dialog = $( "#dialog-form" ).dialog({
+  dialog = $("#dialog-form").dialog({
     autoOpen: false,
     height: 400,
     width: 350,
@@ -43,21 +43,23 @@ $( function() {
     buttons: {
       "Create an account": addUser,
       Cancel: function() {
-        dialog.dialog( "close" );
+        dialog.dialog("close");
       }
     },
     close: function() {
-      form[ 0 ].reset();
-      allFields.removeClass( "ui-state-error" );
+      form[0].reset();
+      allFields.removeClass("ui-state-error");
     }
   });
 
-  form = dialog.find( "form" ).on( "submit", function( event ) {
+  form = dialog.find("form").on("submit", function(event) {
     event.preventDefault();
     addUser();
   });
 
-  $( "#create-user" ).button().on( "click", function() {
-    dialog.dialog( "open" );
-  });
-} );
+  $("#create-user")
+    .button()
+    .on("click", function() {
+      dialog.dialog("open");
+    });
+});
